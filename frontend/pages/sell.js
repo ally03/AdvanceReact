a713603ca1ee570fa185c0300906f90a5cd2e658
Alid Didarali <aliddidarali@Alids-MacBook-Pro.local>
2019-01-23 19:35:38 +0530

    init prisma

--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,9 +1,8 @@
-import Link from 'next/link';               // import link componets from the next
 
 
 const Sell = props => (             // no need to import as next will take care of it. 
     <div>                           
-        <p>Sell!!!!</p>
+        <p>this is for Sell!</p>
     </div>
 );                                 // this is a function components
 
